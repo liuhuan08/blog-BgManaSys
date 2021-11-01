4ef98f187f87bc8556e45ab089573e4729d95d64
liuhuan <1345455674@qq.com>
2021-11-01 09:58:24 +0800

    引入重置样式表normalize.css

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-Vue.config.productionTip = false
+import "./assets/css/normalize.css"; // 重置样式表
+
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+	router,
+	store,
+	render: (h) => h(App),
+}).$mount("#app");
