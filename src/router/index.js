70c51d7e7b7069d56f5835988acd42be73073cd2
liuhuan <1345455674@qq.com>
2021-11-17 13:52:08 +0800

    重复跳转同一路由报警处理

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,14 @@ import Login from "../views/Login.vue"
 
 Vue.use(VueRouter);
 
+/* 解决重复跳转到一个路由 警告你的信息 */
+//获取原型对象上的push函数
+const originalPush = VueRouter.prototype.push
+//修改原型对象中的push方法
+VueRouter.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(err => err)
+}
+
 const routes = [
 	{
 		path: "/",
