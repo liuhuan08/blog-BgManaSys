2634d5a3a9ee566f5d989d525ec6ca487935e6ea
liuhuan <1345455674@qq.com>
2021-11-17 14:27:07 +0800

    路由切换动画

--- src/views/layout/rightSide/components/main.vue
+++ src/views/layout/rightSide/components/main.vue
@@ -1,6 +1,8 @@
 <template>
     <div>
-        <router-view/>
+        <transition name="slide-fade">
+            <router-view/>
+        </transition>
     </div>
 </template>
 
@@ -11,5 +13,17 @@
 </script>
 
 <style lang="less" scoped>
-
+.slide-fade-enter-active {
+  transition: all .3s linear;
+}
+.slide-fade-leave-active {
+  transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.slide-fade-enter, .slide-fade-leave-to {
+  transform: translateX(10px);
+  opacity: 0;
+}
+.slide-fade-leave-to{
+    display: none;
+}
 </style>
