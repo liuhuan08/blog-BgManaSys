38d64a85fae793e1e2da2279c8b18c9afb5a5809
liuhuan <1345455674@qq.com>
2021-11-19 17:12:32 +0800

    获取首页数据接口

---
+++ src/api/user.js
@@ -0,0 +1,10 @@
+import request from './Request'
+
+// 获取首页数据
+export const getData = (bloggerId) => {
+    return request({
+        method: 'get',
+        url: '/blog/back/get-statistics-info',
+        params: { bloggerId }
+    })
+}
