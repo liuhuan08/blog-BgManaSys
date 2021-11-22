5a95a5fc532022a1fe21af7c99723604bbd565c2
liuhuan <1345455674@qq.com>
2021-11-22 09:27:36 +0800

    获取文章列表

--- src/views/articleManage/ArticleList.vue
+++ src/views/articleManage/ArticleList.vue
@@ -1,15 +1,43 @@
 <template>
-    <div>
-        文章列表
+    <div class="article-list-page">
+        
     </div>
 </template>
 
 <script>
-    export default {
-        
+import { getArticleList } from "@/api/article";
+import local from "@/utils/local"
+
+export default {
+    data() {
+        return {
+            limit: 10,
+            page: 1
+        }
+    },
+    methods: {
+        getData() {
+            let bloggerId = local.get('blog_userinfo').bloggerId;
+            let data = {
+                bloggerId,
+                limit: this.limit,
+                page: this.page
+            }
+            getArticleList(data).then(res => {
+                console.log(res);
+            })
+        }
+    },
+    created() {
+        this.getData();
     }
+}
 </script>
 
 <style lang="less" scoped>
-
+.article-list-page{
+    width: 100%;
+    height: 100%;
+    background-color: #fff;
+}
 </style>
