fd61b0d96c36c6410218e792e8d0768c6ac5fd37
liuhuan <1345455674@qq.com>
2021-11-29 15:58:01 +0800

    新增相册完成

--- src/api/albums.js
+++ src/api/albums.js
@@ -1,10 +1,19 @@
 import request from './Request'
 
-// 获取文章列表
+// 获取相册列表
 export const getAlbumsList = (bloggerId) => {
     return request({
         method: 'get',
         url: '/blog/front/get-albums',
         params: { bloggerId }
+    })
+}
+
+// 新增相册
+export const addAlbums = (data) => {
+    return request({
+        method: 'post',
+        url: '/blog/back/add-album',
+        data
     })
 }
--- src/views/imageManage/ImageManage.vue
+++ src/views/imageManage/ImageManage.vue
@@ -2,6 +2,10 @@
 	<div class="album-page">
 		<div class="title">相册列表</div>
 		<ul class="list">
+			<li class="item add-album" @click="handelAddAlbum">
+				<i class="iconfont icon-add"></i>
+				添加相册
+			</li>
 			<li v-for="v in albumsList" :key="v.id" class="item">
 				<div
 					class="img-wrap"
@@ -16,14 +20,21 @@
 								size="mini"
 								@click="handelEdit(v.id)"
 							>
-								编辑相册
+								进入相册
 							</l-botton>
 							<l-botton
 								type="primary"
 								size="mini"
 								@click="handelEdit(v.id)"
 							>
-								进入相册
+								编辑相册
+							</l-botton>
+							<l-botton
+								type="danger"
+								size="mini"
+								@click="handelEdit(v.id)"
+							>
+								删除相册
 							</l-botton>
 						</div>
 					</transition>
@@ -46,26 +57,78 @@
 				</div>
 			</li>
 		</ul>
+
+		<div class="dialog" v-if="dialogVisible">
+			<i
+				class="iconfont icon-closeCard"
+				@click="dialogVisible = false"
+			></i>
+			<div class="title">新增相册</div>
+			<div class="item">
+				<div class="label">相册名称</div>
+				<l-input
+					class="ipt"
+					v-model="dialogForm.name"
+					:ipt-value="dialogForm.name"
+					type="text"
+					placeholder="请输入相册名称"
+					clearable
+				></l-input>
+			</div>
+			<div class="item">
+				<div class="label">相册封面</div>
+				<l-upload-image
+					:src="dialogForm.albumCover"
+					action="http://api.excellentlld.com/blog/back/upload-image"
+					@on-success="handleAvatarSuccess"
+					:before-upload="beforeAvatarUpload"
+				></l-upload-image>
+			</div>
+			<div class="bottom">
+				<l-botton
+					style="margin-right: 10px"
+					type="primary"
+					size="small"
+					@click="handelConfirm"
+				>
+					确认
+				</l-botton>
+				<l-botton
+					type="info"
+					size="small"
+					@click="dialogVisible = false"
+				>
+					取消
+				</l-botton>
+			</div>
+		</div>
 	</div>
 </template>
 
 <script>
-import { getAlbumsList } from "../../api/albums";
+import { getAlbumsList, addAlbums } from "../../api/albums";
 import local from "@/utils/local";
 import { normalizeDate, normalizeTime } from "../../utils/tools";
 
 import LInput from "@/components/input.vue";
 import LBotton from "@/components/botton.vue";
+import LUploadImage from "@/components/uploadImage.vue";
 
 export default {
 	data() {
 		return {
 			albumsList: [],
+			dialogForm: {
+				name: "",
+				albumCover: "",
+			},
+			dialogVisible: false,
 		};
 	},
 	components: {
 		LInput,
 		LBotton,
+		LUploadImage,
 	},
 	methods: {
 		getAlbum() {
@@ -77,7 +140,42 @@ export default {
 						this.$set(this.albumsList[i], "showInfo", false);
 						this.$set(this.albumsList[i], "showOperation", false);
 					});
-					console.log(this.albumsList);
+				}
+			});
+		},
+		handelAddAlbum() {
+			this.dialogVisible = true;
+		},
+		handleAvatarSuccess(data) {
+			this.dialogForm.albumCover = data.data.url;
+		},
+		beforeAvatarUpload(file) {
+			const isJPG = file.type === "image/jpeg";
+			const isLt2M = file.size / 1024 / 1024 < 2;
+
+			if (!isJPG) {
+				this.Msg("上传头像图片只能是 JPG 格式!", "error", 2000);
+			}
+			if (!isLt2M) {
+				this.Msg("上传头像图片大小不能超过 2MB!", "error", 2000);
+			}
+			return isJPG && isLt2M;
+		},
+		handelConfirm() {
+			let bloggerId = local.get("blog_userinfo").bloggerId;
+			let sendData = {
+				bloggerId,
+				albumName: this.dialogForm.name,
+				albumCover: this.dialogForm.albumCover,
+			};
+			addAlbums(sendData).then((res) => {
+				if (res.status === 200) {
+					this.Msg("新增相册成功！", "success", 1500);
+					this.dialogForm = {
+						name: "",
+						albumCover: "",
+					};
+                    this.dialogVisible = false;
 				}
 			});
 		},
@@ -96,9 +194,10 @@ export default {
 
 <style lang="less" scoped>
 .album-page {
+	position: relative;
 	padding: 20px;
 	width: 100%;
-    height: 100%;
+	height: 100%;
 	background-color: #fff;
 
 	.title {
@@ -183,6 +282,75 @@ export default {
 					}
 				}
 			}
+		}
+
+		.add-album {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			justify-content: center;
+			width: 150px;
+			height: 180px;
+			background-color: #eee;
+			border: 1px dashed #ccc;
+			cursor: pointer;
+
+			.icon-add {
+				font-size: 20px;
+			}
+
+			&:hover {
+				color: #409eff;
+				border: 1px dashed #409eff;
+				box-shadow: 3px 3px 3px #ccc;
+			}
+		}
+	}
+
+	.dialog {
+		position: absolute;
+		top: 40%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		padding: 20px;
+		padding-top: 0;
+		width: 400px;
+		height: 400px;
+		background-color: #fff;
+		box-shadow: 0px 0px 5px #ccc;
+		border-radius: 10px;
+
+		.icon-closeCard {
+			position: absolute;
+			top: 5px;
+			right: 10px;
+			font-size: 20px;
+			cursor: pointer;
+		}
+
+		.title {
+			text-indent: 0;
+		}
+
+		.item {
+			display: flex;
+			margin-top: 20px;
+
+			.label {
+				width: 100px;
+			}
+
+			.ipt {
+				flex: 1;
+			}
+		}
+
+		.bottom {
+			position: absolute;
+			bottom: 20px;
+			right: 20px;
+			display: flex;
+			justify-content: end;
 		}
 	}
 }
