254ed1841d7df5ca55835b9a95209d7b24ede9a6
liuhuan <1345455674@qq.com>
2021-12-13 15:46:26 +0800

    msg提示优化

--- src/utils/msg.js
+++ src/utils/msg.js
@@ -4,10 +4,13 @@ export default{
             let timer;
             if(timer) clearTimeout(timer);
             timer = setTimeout(() => {
+                if(document.querySelector("#l-msg-wrap-2021-12")) document.querySelector("#app").removeChild(document.querySelector("#l-msg-wrap-2021-12"));
+
                 let removeTimer;
                 if(removeTimer) clearTimeout(removeTimer);
         
                 let div = document.createElement("div");
+                div.id = "l-msg-wrap-2021-12";
                 div.className = `msg-wrap ${type}`;
                 div.innerHTML = `
                         <i class="iconfont icon-${type}" style="margin-right: 10px;"></i>
@@ -17,7 +20,7 @@ export default{
                 document.querySelector("#app").appendChild(div);
                 document.querySelector("#app").style.position = 'relative'
         
-                let cssText = "display: flex; align-items: center;position: absolute; top: 50px; left: 50%; transform: translateX(-50%); padding-left: 30px; width: 300px; height: 40px; line-height: 40px; border-radius: 4px; overflow: hidden;";
+                let cssText = "display: flex; align-items: center;position: absolute; top: 50px; left: 50%; transform: translateX(-50%); padding-left: 30px; width: 300px; height: 40px; line-height: 40px; border-radius: 4px; overflow: hidden; animation: all 2s linear;";
                 
                 if (type === "msg") {
                     cssText += "color: #909399; background-color: #edf2fc;";
@@ -32,11 +35,8 @@ export default{
                 document.querySelector(".msg-wrap").style.cssText = cssText;
                 
                 removeTimer = setTimeout(() => {
-                    document.querySelector(".msg-wrap").style.animation = `all 2s linear`;
                     document.querySelector(".msg-wrap").style.opacity = 0;
-                    setTimeout(() => {
-                        document.querySelector("#app").removeChild(div);
-                    }, 2000)
+                    document.querySelector("#app").removeChild(div);
                 }, duration);
             },100)
         };
