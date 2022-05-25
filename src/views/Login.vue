9522f2936e549b4ac601d73612d3c1fdc8b25ef8
liuhuan <liuhuan@anmit.com>
2022-05-25 15:37:53 +0800

    登录界面优化

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,26 +1,22 @@
 <template>
-    <div class="login-page">
+    <div class="login-page" :style="`background-image: url(${bg_url});`">
         <div class="modal">
             <div class="title">blog后台系统登录</div>
-            <p class="item">
-                <i class="iconfont icon-account"></i>
-                <input v-model="account.text" @keyup="toggleShow(account)" @mouseenter="toggleShow(account)" @mouseleave="account.isClear = false" @blur="validate(account)" type="text" :class="account.validate ? '' : 'err'">
-                <i v-if="account.isClear" @click="handleClear(account)" @mouseenter="toggleShow(account)" class="iconfont icon-closeCard"></i>
-                <transition name="fade">
-                    <span v-if="account.validate ? '' : 'err'" class="msg">*长度在 2 到 12 字符*</span>
-                </transition>
-            </p>
-            <p class="item">
-                <i class="iconfont icon-psd"></i>
-                <input v-model="psd.text" @keyup="toggleShow(psd)" @mouseenter="toggleShow(psd)" @mouseleave="psd.isClear = false" @blur="validate(psd)" :type="psd.isClose ? 'password' : 'text'" :class="psd.validate ? '' : 'err'">
-                <i v-if="!psd.isClose && psd.isClear" @click="psd.isClose = !psd.isClose" @mouseenter="toggleShow(psd)" class="iconfont icon-eye-open"></i>
-                <i v-if="psd.isClose && psd.isClear" @click="psd.isClose = !psd.isClose" @mouseenter="toggleShow(psd)" class="iconfont icon-eye-close"></i>
-                <i v-if="psd.isClear" @click="handleClear(psd)" @mouseenter="toggleShow(psd)" class="iconfont icon-closeCard"></i>
-                <transition name="fade">
-                    <span v-if="psd.validate ? '' : 'err'" class="msg">*长度在 2 到 12 字符*</span>
-                </transition>
-            </p>
-            <button @click="handleLogin" class="btn">登录</button>
+            <el-form :model="loginForm" :rules="rules" ref="loginForm" label-width="0px">
+                <el-form-item label="" prop="name">
+                    <el-input v-model="loginForm.name" clearable>
+                        <i slot="prefix" class="iconfont icon-account"></i>
+                    </el-input>
+                </el-form-item>
+                <el-form-item label="" prop="pwd">
+                    <el-input v-model="loginForm.pwd" clearable show-password>
+                        <i slot="prefix" class="iconfont icon-psd"></i>
+                    </el-input>
+                </el-form-item>
+                <el-form-item>
+                    <button @click="handleLogin" class="btn">登录</button>
+                </el-form-item>
+            </el-form>
         </div>
     </div>
 </template>
@@ -29,57 +25,36 @@
 import { Login } from "../api/Login";
 import local from "../utils/local"
 
-    export default {
-        data() {
-            return {
-                account: {
-                    type: 'account',
-                    text: '',
-                    isClear: false,
-                    validate: true
-                },
-                psd: {
-                    type: 'psd',
-                    text: '',
-                    isClose: true,
-                    isClear: false,
-                    validate: true
-                },
-            }
+export default {
+    data() {
+        return {
+            loginForm: {
+                name: '',
+                pwd: ''
+            },
+            rules: {
+                name: [
+                    { required: true, message: '请输入账号', trigger: 'blur' },
+                    { min: 2, max: 12, message: '长度在 2 到 12 个字符', trigger: 'blur' }
+                ],
+                pwd: [
+                    { required: true, message: '请输入密码', trigger: 'blur' },
+                    { min: 2, max: 12, message: '长度在 2 到 12 个字符', trigger: 'blur' }
+                ]
+            },
+            bg_url: ''
+        }
+    },
+    methods: {
+        setBgIamge() {
+            const num = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+            this.bg_url = require(`../assets/imgs/login-bg/bg_0${num}.jpeg`)
         },
-        methods: {
-            // 控制清空按钮显示隐藏
-            toggleShow(val) {
-                if(val.text.toString().length > 0) {
-                    val.isClear = true;
-                }else {
-                    val.isClear = false;
-                };
-            },
-            // 清空
-            handleClear(val) {
-                val.text = '';
-                val.isClear = false;
-                this.validate(val);
-            },
-            // 前端验证
-            validate(val) {
-                if(val.type === 'account') {
-                    let res;
-                    val.text.toString().length > 1 && val.text.toString().length < 13 ? res = true : res = false;
-                    this.account.validate = res;
-                }else {
-                    let res;
-                    val.text.toString().length > 1 && val.text.toString().length < 13 ? res = true : res = false;
-                    this.psd.validate = res;
-                };
-            },
-            // 登录
-            handleLogin() {
-                this.validate(this.account);
-                this.validate(this.psd);
-                if(this.account.validate && this.psd.validate) {
-                    Login({name: this.account.text, pwd: this.psd.text}).then(res => {
+        // 登录
+        handleLogin() {
+            this.$refs.loginForm.validate((valid) => {
+                if (valid) {
+                    Login(this.loginForm).then(res => {
                         if(res.status === 200) {
                             let data = res.data.data;
                             local.set("blog_t&k", data.token);
@@ -92,26 +67,29 @@ import local from "../utils/local"
                                 createTime: data.createTime
                             }
                             local.set("blog_userinfo", info);
-
-                            this.Msg('登录成功 ~', 'success', 2000);
-                            setTimeout(() => {
-                                this.$router.push({path:'/'})
-                            }, 1000);
+                            this.$modal.msgSuccess('登录成功 ~');
+                            this.$router.push({path:'/'})
                         }
                     })
-
-                };
-            }
-        },
-        mounted() {
-            // 回车登录
-            window.addEventListener('keyup', (e) => {
-                if(e.key === 'Enter') {
-                    this.handleLogin();
-                };
+                } else {
+                    console.log('error submit!!');
+                    return false;
+                }
             });
         }
+    },
+    created() {
+        this.setBgIamge()
+    },
+    mounted() {
+        // 回车登录
+        window.addEventListener('keyup', (e) => {
+            if(e.key === 'Enter') {
+                this.handleLogin();
+            };
+        });
     }
+}
 </script>
 
 <style lang="less" scoped>
@@ -120,6 +98,8 @@ import local from "../utils/local"
     width: 100%;
     height: 100%;
     background-color: #999;
+    background-repeat: no-repeat;
+    background-size: cover;
 
     .modal{
         display: flex;
@@ -138,66 +118,8 @@ import local from "../utils/local"
             line-height: 30px;
         }
 
-        .item{
-            position: relative;
-            margin-top: 20px;
-            z-index: 999;
-
-            input{
-                width: 400px;
-                height: 40px;
-                text-indent: 30px;
-                border-radius: 5px;
-                color: #333;
-                border: 1px solid #eee;
-                box-shadow: 0px 0px 1px #eee;
-
-                &:focus{
-                    border: 1px solid #409eff;
-                    box-shadow: 0px 0px 1px #409eff;
-                }
-            }
-
-            .err{
-                border: 1px solid #f56c6c;
-            }
-
-            .iconfont{
-                color: #666;
-            }
-
-            .icon-account, .icon-psd{
-                position: absolute;
-                top: 50%;
-                left: 10px;
-                transform: translateY(-50%);
-            }
-
-            .icon-eye-open, .icon-eye-close{
-                position: absolute;
-                top: 50%;
-                right: 30px;
-                transform: translateY(-50%);
-                cursor: pointer;
-                z-index: 9999;
-            }
-
-            .icon-closeCard{
-                position: absolute;
-                top: 50%;
-                right: 10px;
-                transform: translateY(-50%);
-                cursor: pointer;
-            }
-
-            .msg{
-                position: absolute;
-                bottom: -14px;
-                left: 0;
-                font-size: 12px;
-                line-height: 12px;
-                color: #f34134;
-            }
+        .iconfont{
+            color: #666;
         }
 
         .btn{
@@ -219,9 +141,14 @@ import local from "../utils/local"
 }
 
 .fade-enter-active, .fade-leave-active {
-  transition: opacity .5s;
+    transition: opacity .5s;
 }
-.fade-enter, .fade-leave-to {
-  opacity: 0;
+.fade-enter {
+    transform: translateX(20px);
+    opacity: 0;
+}
+.fade-leave-to {
+    transform: translateX(-20px);
+    opacity: 0;
 }
 </style>
