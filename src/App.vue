bac80a596a8e34466d602e7eaa7516ce151a6798
liuhuan <1345455674@qq.com>
2023-11-27 10:01:53 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,7 @@
 <template>
-	<div id="app">
-		<router-view />
-	</div>
+  
 </template>
 
-<style lang="less">
-#app {
-	width: 100%;
-	height: 100%;
-	min-width: 1450px;
-}
+<style lang="scss">
+
 </style>
